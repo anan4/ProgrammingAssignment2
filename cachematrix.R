5cb3c1998fe5ffb733026f41fb52bfad2dc388c3
anan4 <39746052+anan4@users.noreply.github.com>
2018-06-12 19:16:32 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Coursera course Assignment 2
+## the nest 2 functions do cache of the inversal of a matrix (solve function)
+## assuming that the input matrix has inverse matrix
 
-## Write a short comment describing this function
+## This function creates a special "matrix" that allows to cache its inversal matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) s <<- solve
+  getSolve <- function() s
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of a matrix first time and returns the value of the cache afterwards
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  s <- x$getSolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setSolve(s)
+  s
 }
